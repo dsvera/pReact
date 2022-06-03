7db40d8ad293965b3a807c2dd75ee8a47de2374f
Vera DOS SANTOS <vera.dossantos@newbound.eu>
2022-06-03 17:55:00 +0200

    Fin du projet

---
+++ mon-app/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus illum expedita numquam tempore cum aliquam sed laboriosam molestias, aspernatur maxime? Quia alias modi explicabo ex pariatur accusamus vero natus! Facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo impedit corporis labore, nemo voluptas sequi illum accusantium repellendus itaque hic minus dolorum reiciendis expedita a unde nihil nisi, ipsa culpa?</p>
+        </div>
+    );
+};
+
+export default About;
