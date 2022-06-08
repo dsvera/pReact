27423d9937fe24fb02c0a07f733fce8f21469fcf
Vera DOS SANTOS <vera.dossantos@newbound.eu>
2022-06-08 13:58:00 +0200

    fin du tuto node

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+//const mongoose = require('mongoose');
+
+//mongoose.set('useFindAndModify', false);
 
 app.use(bodyParser.json());
+//on donne l'acces a tout le monde avec le paquer cors 
+// Sinon on doit mettre app.use(cors({origin: 'https://nanaplouf.fr'}))
+app.use(cors());
 app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500'));
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId
 
 const { PostsModel } = require('../models/postsModel');
 
@@ -21,5 +22,41 @@ router.post('/', (req, res) => {
         else console.log("error creating new data :" + err);
     })
 });
+
+router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknow : " + req.params.id)
+
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord },
+        { new: true },
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log("Update error : " + err);
+        }
+    )
+});
+
+router.delete("/:id", (req, res) => {
+    // si tu ne trouve pas envoi un mess d'erreur
+    if (!ObjectID.isValid(req.params.id))
+        return res.status(400).send("ID unknow : " + req.params.id)
+
+    PostsModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log("Delete error :" + err)
+        }
+    );
+});
+
+
 
 module.exports = router
